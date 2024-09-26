34a1b1ef5513a44d1c74c18401b8e631375494df
asanchez89 <asanchez@deltare.net>
2024-09-25 21:36:07 -0400

    Se implementa la clase del servicio

--- src/main/java/com/curso/estudiantes/servicio/EstudianteServicio.java
+++ src/main/java/com/curso/estudiantes/servicio/EstudianteServicio.java
@@ -1,4 +1,34 @@
 package com.curso.estudiantes.servicio;
 
+import com.curso.estudiantes.modelo.Estudiante;
+import com.curso.estudiantes.repositorio.EstudianteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class EstudianteServicio implements IEstudianteServicio{
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+        return estudiante;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.save(estudiante);
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.delete(estudiante);
+    }
 }
